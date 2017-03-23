b5795341a757aff250f496d739b3396010cfa2b1
HesongHuang <huanghesong3231@yahoo.com>
2017-03-23 15:03:24 -0400

    Update journal_name.py
    put the data into a diction,like {"@article1": "hardin2014clinical,", "  title=1": "The clinical and genetic features of COPD-asthma overlap syndrome},", "  author=1": "Hardin, Megan and Cho, Michael and McDonald, Merry-Lynn and Beaty, Terri and Ramsdell, Joe and Bhatt, Surya and Van Beek, Edwin JR and Make, Barry J and Crapo, James D and Silverman, Edwin K and others},", "  journal=1": "European Respiratory Journal},", "  volume=1": "44},", "  number=1": "2},", "  pages=1": "341--350},", "  year=1": "2014},", "  publisher=1": "Eur Respiratory Soc}", "@article2": "lasso2014plus,",} the number "=1"represent they are from the same article.
    Save the diction into a new json file called "Bibtext_format.json"
    I have to say, when you input -c 100, you actually get 200 results,besauce every page of google scholar shows 20 results.
    So the number of journal is true.

--- journal_name.py
+++ journal_name.py
@@ -3,7 +3,14 @@ plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import pdb
+import types
 
+def store(data,file_name):
+    with open(file_name, 'w') as json_file:
+        json_file.write(json.dumps(data))
+
+        
 def get_json():
 	jour=0
 	conf=0
@@ -11,25 +18,35 @@ def get_json():
 	diction={}
 	number=[]
 	name=[]
-	with open('bibtex1.json') as json_file:
-		data = json.load(json_file)
+	bibdic={}
+	n=0
+	with open('bibtex.json') as json_file:
+		data = json.load(json_file) #type of data is list
 		for a in data:
-			b = a.split('\n')
-			for m in b: 
-				if "journal={" in m:
+			c=a.split("\n")  #type of data is list
+			n=n+1
+			for mate in c:
+				d=mate.split("{")
+				if len(d)==2:   #type of d[0] is srtings
+					m=str(n)
+					d[0]=d[0]+m
+					bibdic[d[0]]=d[1]
+		store(bibdic,"Bibtex_format.json")
+		for a in bibdic:
+				if "journal=" in a:
 					jour=jour+1
-					diction[m]= diction.get(m,0)+1
-				elif "booktitle={" in m:
+					diction[bibdic[a]]= diction.get(bibdic[a],0)+1
+				elif "booktitle=" in a:
 					conf= conf+1
-					diction[m]= diction.get(m,0)+1
-				elif "title" in m:
-					list.append(m) 
+					diction[bibdic[a]]= diction.get(bibdic[a],0)+1
+				elif "title" in a:
+					list.append(bibdic[a]) 
 		print ("the number of journal is:",jour)
 		print ("the number of conference is:",conf)
-		print (list)
+		store(list,"title.json")
 		for key in diction:
 			number.append(diction[key])
-			key = key.replace("journal={","")
+			key = key.replace("journal=","")
 			key = key.replace("},","")
 			name.append(key)
 	plt.rcdefaults()
@@ -49,3 +66,6 @@ def get_json():
 	
 if __name__=="__main__":
 	get_json()
+	
+            
+               
